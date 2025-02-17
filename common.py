28b2338c836ad5b22e2fa98461e9939d533eef25
Gregory <unniat03@gmail.com>
2025-02-16 20:53:29 -0600

    comment common.py

--- common.py
+++ common.py
@@ -1,3 +1,4 @@
+# Importing necessary modules
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -5,9 +6,13 @@ from datetime import datetime
 import os
 import logging
 
+
+# Initializing the Flask application
 app = Flask(__name__, instance_relative_config=True)
 CORS(app)
+# Defining the database file path inside the instance folder
 db_path = os.path.join(app.instance_path, 'news.db')
+# Configuring the flask app inside the instance folder
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -16,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 os.makedirs(app.instance_path, exist_ok=True)
-
+# Defining the Article model (table) to store news articles
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
@@ -26,29 +31,35 @@ class Article(db.Model):
     processed = db.Column(db.Boolean, default=False)
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
 
+# Defining the Topic model (table) to store topics/categories of articles
 class Topic(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     #articles = db.relationship('Article', secondary='articles_topics')
 
+# Defining the Ranking model (table) to rank topics
 class Ranking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     topics = db.relationship('Topic', secondary='rankings_topics')
 
+# Defining a linking table between Rankings and Topics
 class RankingsTopics(db.Model):
     __tablename__ = 'rankings_topics'
     ranking_id = db.Column(db.Integer, db.ForeignKey('ranking.id'), primary_key=True)
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), primary_key=True)
     rank_order = db.Column(db.Integer)
 
+
+# Defining the Insight model (table) to store insights related to topics
 class Insight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
     content = db.Column(db.Text)
     insight_type = db.Column(db.String(50))
 
+# Defining the SocialMediaPost model (table) to store social media posts about topics
 class SocialMediaPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
@@ -62,6 +73,7 @@ class SocialMediaPost(db.Model):
 #     article_id = db.Column(db.Integer, db.ForeignKey('article.id'), primary_key=True)
 #     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), primary_key=True)
 
+# Function to initialize the database
 def init_database():
     with app.app_context():
         db.drop_all()
