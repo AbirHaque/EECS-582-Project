cfe5d9a3754aea9473c74204ea4c8390e3997eb7
Gregory <unniat03@gmail.com>
2025-02-16 21:06:19 -0600

    comment message_bus.py

--- message_bus.py
+++ message_bus.py
@@ -1,9 +1,11 @@
 import queue
 
+# Creating separate queues for different types of messages
 ranking_queue = queue.Queue()
 social_queue = queue.Queue()
 insights_queue = queue.Queue()
 
+# Function to send messages to the appropriate queues
 def send_message(queue_name, message):
     if queue_name == 'topics_generated':
         ranking_queue.put({'queue': queue_name, 'body': message})
